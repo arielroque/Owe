648b2353cf4c262ea91e42f9f92a06cddcb2c2c1
Ariel <ariel.luz@ccc.ufcg.edu.br>
2021-01-16 17:49:44 -0300

    Add font,color_theme and splash screen

---
+++ lib/owe/utils/color_theme.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+///COLOR PALLETE
+
+class ColorThemeApp {
+  static const Color primaryColor = Color(0xFF3366CC);
+  static const Color secondColor = Color(0xFFEA5F9E);
+  static const Color tertiaryColor = Color(0xFF35CCD3);
+}
